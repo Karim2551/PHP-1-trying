d0577aa48eb48379e47e4ee1c9462c07566d2694
Karim2551 <63285946+Karim2551@users.noreply.github.com>
2020-04-08 15:30:47 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	Hello World!
+	<?php echo "Hello php World"?> 
+	<?php echo "TS1902 is the best group"?> 
+</body>
+</html>
